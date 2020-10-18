39553022a71c9bf6aebb27ce9cdc70ba0392dbf2
leemichalee <leeisr@gmail.com>
2020-10-18 13:39:14 -0400

    initialized menu item

---
+++ app/models/menu_item.rb
@@ -0,0 +1,16 @@
+class MenuItem
+    attr_accessor :price
+    attr_reader :restaurant, :recipe
+    @@all = []
+
+    def initialize(restaurant, recipe, price)
+        @restaurant = restaurant
+        @recipe = recipe
+        @price = price.to_f
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
